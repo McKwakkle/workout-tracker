d391ada01f767e98cacb23696afd8e7c3bc39b3c
McKwakkle <kellanmcn@gmail.com>
2026-02-23 18:45:29 +0000

    Fixed the scroll to top button on mobile

--- src/App.js
+++ src/App.js
@@ -75,45 +75,48 @@ function App() {
             </li>
           </ul>
         </nav>
+        <div className="content-wrapper">
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/log" element={<LogWorkout />} />
+              <Route path="/progress" element={<Progress />} />
+              <Route path="/photos" element={<Photos />} />
+              <Route path="/history" element={<WorkoutHistory />} />
+              <Route path="/calendar" element={<Calendar />} />
+            </Routes>
+          </main>
 
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/log" element={<LogWorkout />} />
-            <Route path="/progress" element={<Progress />} />
-            <Route path="/photos" element={<Photos />} />
-            <Route path="/history" element={<WorkoutHistory />} />
-            <Route path="/calendar" element={<Calendar />} />
-          </Routes>
-        </main>
-
-        <footer className="site-footer">
-          <div className="footer-links">
-            <a
-              href="https://github.com/McKwakkle"
-              target="_blank"
-              rel="noreferrer"
-              className="footer-link"
-            >
-              <i className="fa-brands fa-github"></i>
-              Github
-            </a>
-            <a
-              href="https://www.linkedin.com/in/kellan-mc-naughton-906653120/"
-              target="_blank"
-              rel="noreferrer"
-              className="footer-link"
-            >
-              <i className="fa-brands fa-linkedin"></i>
-              LinkedIn
-            </a>
-            <a href="#" className="footer-link footer-link-disabled">
-              <i className="fa-solid fa-briefcase"></i>
-              Portfolio (Coming Soon)
-            </a>
-          </div>
-          <p className="footer-credit">Built by Kellan "Obelix" Mc Naughton</p>
-        </footer>
+          <footer className="site-footer">
+            <div className="footer-links">
+              <a
+                href="https://github.com/McKwakkle"
+                target="_blank"
+                rel="noreferrer"
+                className="footer-link"
+              >
+                <i className="fa-brands fa-github"></i>
+                Github
+              </a>
+              <a
+                href="https://www.linkedin.com/in/kellan-mc-naughton-906653120/"
+                target="_blank"
+                rel="noreferrer"
+                className="footer-link"
+              >
+                <i className="fa-brands fa-linkedin"></i>
+                LinkedIn
+              </a>
+              <a href="#" className="footer-link footer-link-disabled">
+                <i className="fa-solid fa-briefcase"></i>
+                Portfolio (Coming Soon)
+              </a>
+            </div>
+            <p className="footer-credit">
+              Built by Kellan "Obelix" Mc Naughton
+            </p>
+          </footer>
+        </div>
       </div>
     </Router>
   );
